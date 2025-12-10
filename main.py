869171cd38893bf82cfe4dc5438e589f6260c837
Yehan Pemarathne <yehancha@gmail.com>
2025-12-10 18:20:19 +0530

    Use response structures

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
@@ -8,10 +11,31 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 
+
+class Source(BaseModel):
+    """
+    Schema for a source used by the agent
+    """
+
+    url: str = Field(description="The url of the source")
+
+
+class AgentResponse(BaseModel):
+    """
+    Schema for the response of the agent
+    """
+
+    answer: str = Field(description="The agent'sanswer to the question")
+    sources: List[Source] = Field(
+        default_factory=list,
+        description="The sources used to generate the answer to the question",
+    )
+
+
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 # llm = ChatOllama(model="qwen3:0.6b")
 tools = [TavilySearch()]
-agent = create_agent(llm, tools)
+agent = create_agent(llm, tools, response_format=AgentResponse)
 
 
 def main():

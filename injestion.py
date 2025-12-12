632845b0f7171ca0bf47e79b6b43e8f5ee78fc95
Yehan Pemarathne <yehancha@gmail.com>
2025-12-12 10:08:29 +0530

    Injest files

---
+++ injestion.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv() 
+
+if __name__ == "__main__":
+    print("Injesting...")
+
+    loader = TextLoader("mediumblog1.txt")
+    documents = loader.load()
+
+    print("Splitting...")
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+
+    print(f"Created {len(texts)} chunks")
+
+    embeddings = OllamaEmbeddings(model="qwen3-embedding:0.6b")
+
+    print("Start injestion...")
+
+    PineconeVectorStore.from_documents(
+        texts,
+        embeddings,
+        index_name=os.getenv("INDEX_NAME"),
+    )
+
+    print("Injestion complete")
